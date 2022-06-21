332f131226aecde60b0db2a8def6b2b80e24d1c5
josefernandes22 <jcastrosa22@gmail.com>
2022-06-21 19:49:05 -0300

    alteração hora trabalhada

--- 01_questao15.py
+++ 01_questao15.py
@@ -1,10 +1,10 @@
 
 #15) Crie um programa que leia o número de dias trabalhados em um mês e mostre o
-#salário de um funcionário, sabendo que ele trabalha 8 horas por dia e ganha R$25
+#salário de um funcionário, sabendo que ele trabalha 10 horas por dia e ganha R$25
 #por hora trabalhada.
 
 dias = float(input('Informe a quantidade de dias trabalhados no mês: '))
 
-salario = (25*8)*dias
+salario = (25*10)*dias
 
 print('Ao final do mês, o funcionário deverá receber: ' + str(salario))
